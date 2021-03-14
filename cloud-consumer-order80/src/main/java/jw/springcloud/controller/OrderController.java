039636a50831ac34dbbad8bf38d5194231b4b66c
jiangwei <734145091@qq.com>
2021-03-14 15:25:44 +0800

    Hystrix

--- cloud-consumer-order80/src/main/java/jw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/jw/springcloud/controller/OrderController.java
@@ -1,14 +1,20 @@
 package jw.springcloud.controller;
 
+import jw.springcloud.lb.LoadBalancer;
 import jw.springcloud.entities.CommonResult;
 import jw.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @auther zzyy
@@ -24,11 +30,12 @@ public class OrderController
 
     @Resource
     private RestTemplate restTemplate;
-//
-//    @Resource
-//    private LoadBalancer loadBalancer;
-//    @Resource
-//    private DiscoveryClient discoveryClient;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment)
@@ -42,34 +49,34 @@ public class OrderController
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 
-//    @GetMapping("/consumer/payment/getForEntity/{id}")
-//    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
-//    {
-//        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
-//
-//        if(entity.getStatusCode().is2xxSuccessful()){
-//            return entity.getBody();
-//        }else{
-//            return new CommonResult<>(444,"操作失败");
-//        }
-//    }
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
+    {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
 
-//    @GetMapping(value = "/consumer/payment/lb")
-//    public String getPaymentLB()
-//    {
-//        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-//
-//        if(instances == null || instances.size() <= 0)
-//        {
-//            return null;
-//        }
-//
-//        ServiceInstance serviceInstance = loadBalancer.instances(instances);
-//        URI uri = serviceInstance.getUri();
-//
-//        return restTemplate.getForObject(uri+"/payment/lb",String.class);
-//
-//    }
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB()
+    {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if(instances == null || instances.size() <= 0)
+        {
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalancer.instance(instances);
+        URI uri = serviceInstance.getUri();
+
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+
+    }
 
     // ====================> zipkin+sleuth
 //    @GetMapping("/consumer/payment/zipkin")
--- cloud-consumerconsul-order80/src/main/java/jw/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumerconsul-order80/src/main/java/jw/springcloud/config/ApplicationContextConfig.java
@@ -1,21 +1,21 @@
-package jw.springcloud.contfig;
+package jw.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @auther zzyy
- * @create 2020-02-18 17:27
+ * @create 2020-02-19 15:20
  */
 @Configuration
 public class ApplicationContextConfig
 {
     @Bean
-//    @LoadBalanced
+    @LoadBalanced
     public RestTemplate getRestTemplate()
     {
         return new RestTemplate();
     }
 }
-//applicationContext.xml <bean id="" class="">
